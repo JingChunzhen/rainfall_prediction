2e51b403cf988b60996e921b701f65a7d14039ff
JingChunzhen <1051949460@qq.com>
2017-04-22 17:43:08 +0800

    modified 3 files

--- data_parse.py
+++ data_parse.py
@@ -1,72 +1,138 @@
-import tensorflow as tf
-import tflearn
+import linecache
+import os
+
 import h5py
 import numpy as np
+import tensorflow as tf
+
+import basic
 
 # split data use command in linux : split
 # train model use batch data
-# get the splited raw data from file 
-# CIKM data shape : 
-'''
-batch ->
-    id0, label0, radar_map0
-    id1, label1, radar_map1
-    ...
-    idn, labeln, radar_mapn
+# get the splited raw data from file
+# CIKM data shape :
+# batch ->
+#     id0, label0, radar_map0
+#     id1, label1, radar_map1
+#     ...
+#     idn, labeln, radar_mapn
 
-radar_map ->
-    shape: 15*4*101*101
-'''
-# linux command cat train.txt | head 100 > sample 
+# radar_map ->
+#     shape: 15*4*101*101
+# linux command cat train.txt | head -100 > sample
 # each line get a data for id, label, and radar_map data type : str
-# the command above get a sample which has 100 data of data file 
+# the command above get a sample which has 100 data of data file
+# convert the data type int <-> str and ASCII int <-> str
 
-# parse the data get from the sample 
 
-def parse_data(batch_size):
-    filename = './data/CIKM2017_train/data_new/CIKM2017_train/sample'
-    with open(filename, 'r') as f:
-        raw_x = []
-        raw_y = []
-        count = 0
-        #for line in f.readlines():            
-        for line in f:
-            id, label, radar_map = line.split(',')  # type(radar_map) == str                 
-            radar_map = radar_map.split(' ') # get 612060
-            raw_y.append(label)
-            raw_x.append(radar_map)
-            count += 1
-            if count == batch_size:
-                break
+def get_dir(dir_count):
+    filename = './data/CIKM2017_train/split_file'
+    if dir_count < 10:
+        filename += '0'
+        filename += str(dir_count)
+    else:
+        filename += str(dir_count)
+    return filename
+
+# get raw_x and  raw_y
+
+
+# dir_count count from 0, batch_count count from 0
+def generate_batch(dir_count, batch_count, batch_size):
+    filename = get_dir(dir_count)
+
+    raw_x = []
+    raw_y = []
+
+    for i in range(1, batch_size + 1):
+        line_num = batch_count * batch_size + i  # line num count from 1
+        line = linecache.getline(filename, line_num)  # cant read num 0 line
+        id, label, radar_map = line.split(',')
+        radar_map = radar_map.split(' ')  # get 612060
+        raw_y.append(label)
+        raw_x.append(radar_map)
+
+    raw_x = np.asarray(raw_x, dtype=np.float)
+    raw_y = np.asarray(raw_y, dtype=np.float)
+
+    raw_x = np.reshape(raw_x, [batch_size, 15, 4, 101, 101])
+    raw_x = np.transpose(raw_x, [1, 0, 3, 4, 2])
+    raw_y = np.reshape(raw_y, [batch_size, 1])
+
     return raw_x, raw_y
 
-# raw_x need to be normalized see tensorflow wrt normalization 
-def preprocess(batch_size):
-    raw_x, raw_y = parse_data(batch_size)
-    raw_x = np.asarray(raw_x, dtype=np.float)    
-    raw_y = np.asarray(raw_y, dtype=np.float)    
-    raw_x = np.reshape(raw_x, [batch_size, 15, 4, 101, 101])  # 0, 1, 2, 3, 4
-    raw_x = np.transpose(raw_x, [1, 0, 3, 4, 2])    
-    return raw_x, raw_y
+
+# load and store weights and biases data to the hdf5
+
+
+def load_params():
+    if is_empty():
+        return basic.weights_conv, basic.biases_conv, basic.weights_lstm, basic.biases_lstm
+    else:
+        weights_conv = {}
+        biases_conv = {}
+
+        weights_lstm = {}
+        biases_lstm = {}
+
+        h5f = h5py.File('./hdf5_data/hdf5_data.h5', 'r')
+
+        weights_conv['w1'] = h5f['w1']
+        weights_conv['w2'] = h5f['w2']
+        weights_conv['wf'] = h5f['wf']
+
+        biases_conv['b1'] = h5f['b1']
+        biases_conv['b2'] = h5f['b2']
+        biases_conv['bf'] = h5f['bf']
+
+        weights_lstm['out'] = h5f['weights_out']
+        biases_lstm['out'] = h5f['biases_out']
+
+        h5f.close()
+
+        return weights_conv, biases_conv, weights_lstm, biases_lstm
+
+
+def is_empty():
+    filename = './hdf5_data/hdf5_data.h5'
+    flag = True
+    if os.path.exists(filename):
+        if os.path.getsize(filename):  # != 0
+            flag = False
+        else:
+            flag = True
+    else:
+        flag = True
+
+    return flag
+
+
+# stored conv params and lstm params into the hdf5 file
+
+
+def store_param(weights_conv, biases_conv, weights_lstm, biases_lstm):
+    h5f = h5py.File('./hdf5_data/hdf5_data.h5', 'w')
+
+    h5f.create_dataset('w1', data=weights_conv['w1'])
+    h5f.create_dataset('w2', data=weights_conv['w2'])
+    h5f.create_dataset('wf', data=weights_conv['wf'])
+
+    h5f.create_dataset('b1', data=biases_conv['b1'])
+    h5f.create_dataset('b2', data=biases_conv['b2'])
+    h5f.create_dataset('bf', data=biases_conv['bf'])
+
+    h5f.create_dataset('weights_out', data=weights_lstm['out'])
+    h5f.create_dataset('biases_out', data=biases_lstm['out'])
+
+    h5f.close()
+
+# update params
+
+
+def update_param(weights_conv, biases_conv, weights_lstm, biases_lstm):
+    filename = './hdf5_data/hdf5_data.h5'
+
+    if os.path.exists(filename):
+        os.remove(filename)
+    store_param(weights_conv, biases_conv, weights_lstm, biases_lstm)
     pass
-
-if __name__ == '__main__':     
-    '''           
-    raw_x, raw_y = preprocess(10) # raw_x.shape: (15, 10, 101, 101, 4)
-    print(raw_x[0].shape)  # shape: (10, 101, 101, 4)        
-    print(raw_y.shape)     
-    '''
-    import time
-    start = time.time()
-    raw_x, raw_y = preprocess(10)
-    end = time.time()
-    print(raw_x.shape)
-    print(end - start)
-    
-
-    
-    
-
-            
-        
-
--- model.py
+++ model.py
@@ -1,61 +1,108 @@
-import tensorflow as tf
 import numpy as np
-import h5py
-import basic
-import data_parse
+import tensorflow as tf
 from tensorflow.contrib import rnn
 
-#weights_conv, biases_conv = basic.load_params()
+import basic
+import data_parse
 
-# load and update weights and biases
-# batch train use SGD 
+# split the data into 32 parts each part has 100 lines of data
+# get each batch in each split data
+# update params once the split data training over
+# then the validation test commence
+# and the cycle is repeated till the epochs end
 
-def process(learning_rate):
-    
-    raw_x, raw_y = data_parse.preprocess(10)    
-    weights_conv = basic.weights_conv
-    biases_conv = basic.biases_conv
+global dir_num = 32
 
-    weights_lstm = basic.weights_lstm
-    biases_lstm = basic.biases_lstm
+weights_conv = {
+    'w1': tf.Variable(tf.random_normal([5, 5, 4, 1024])),  # after maxpool2d
+    'w2': tf.Variable(tf.random_normal([5, 5, 1024, 512])),
+    'wf': tf.Variable(tf.random_normal([5 * 5 * 512, 1000]))
+}
 
-    x = tf.placeholder('float', [10, 101, 101, 4])
-    y = tf.placeholder('float', [10, 1])
-    conv_x = basic.conv_process(x, weights_conv, biases_conv, dropout=0.75)
+biases_conv = {
+    'b1': tf.Variable(tf.random_normal([1024])),
+    'b2': tf.Variable(tf.random_normal([512])),
+    'bf': tf.Variable(tf.random_normal([1000]))
+}
 
-    x_lstm = tf.placeholder('float', [15, 10, 1000])    
+weights_lstm = {
+    'out': tf.Variable(tf.random_normal([128, 1]))
+}
+
+biases_lstm = {
+    'out': tf.Variable(tf.random_normal([1]))
+}
+
+model_path = './model_data/model.ckpt'
+
+def process(learning_rate, batch_size, epochs):
+    assert 100 % batch_size == 0
+    assert batch_size <= 100    
+
+    x = tf.placeholder('float', [batch_size, 101, 101, 4])
+    y = tf.placeholder('float', [batch_size, 1])
+    x_conv = basic.conv_process(x, weights_conv, biases_conv, dropout=0.75)
+
+    x_lstm = tf.placeholder('float', [15, batch_size, 1000])
     pred = basic.lstm_process(x_lstm, weights_lstm, biases_lstm)
 
-    RMSE = tf.sqrt(tf.reduce_mean(tf.square(raw_y - pred))) # wrt raw_y
+    RMSE = tf.sqrt(tf.reduce_mean(tf.square(raw_y - pred)))  # wrt raw_y
 
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(RMSE)
-
+    optimizer = tf.train.AdamOptimizer(
+        learning_rate=learning_rate).minimize(RMSE)
+        
     init = tf.initialize_all_variables()
     
-    with tf.Session() as sess:  # a loop supposed to be here 
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
         sess.run(init)
+
+        batch_num = 100 / batch_size
+        epoch = 0
+
+        if os.path.getsize('./model_data/model.ckpt') != 0:
+            saver.restore(sess, './model_data/model.ckpt')
         
-        train_x = []
-        for i in range(15):
-            _x = sess.run([conv_x], feed_dict={x:raw_x[i]})
-            train_x.append(_x)
-        train_x = np.asarray(train_x)        
-        print(train_x.shape)  # get 15 1 10 1000
-        train_x = np.transpose(train_x, [1, 0, 2, 3])
-        print(train_x.shape)
-        print(train_x[0].shape)  # get 15 10 1000
+        while epoch < epochs:
+            for dir_count in range(dir_num):
+                for batch_count in range(batch_num):
 
-        train_x = np.asarray(train_x[0])
-        raw_y = np.reshape(raw_y, [10, 1])
-        feed_dict = {x_lstm:train_x, y:raw_y}  
-        RMSE, _ = sess.run([RMSE, optimizer], feed_dict=feed_dict)          
-        print(RMSE)
-        pass
+                    raw_x, raw_y = data_parse.generate_batch(
+                        dir_count, batch_count, batch_size)
+
+                    train_x = []
+
+                    for i in range(15):
+                        _x = sess.run([x_conv], feed_dict={x: raw_x[i]})
+                        train_x.append(_x)
+
+                    train_x = np.asarray(train_x)
+                    print(train_x.shape)  # get 15 1 10 1000
+                    train_x = np.transpose(train_x, [1, 0, 2, 3])
+
+                    print(train_x.shape)
+                    print(train_x[0].shape)  # get 15 10 1000
+
+                    train_x = np.asarray(train_x[0])
+
+                    RMSE, _ = sess.run(
+                        [RMSE, optimizer],
+                        feed_dict={x_lstm: train_x, y: raw_y}
+                    )
+                    print(RMSE)
+
+                # validation
+                # data_parse.update_param(
+                #     weights_conv, biases_conv, weights_lstm, biases_lstm)
+                model_path = './model_data/'
+                saver.save(sess, model_path)
+                # validation test
+                pass  # end of for
+            epoch += 1
+        pass  # end of while
+
+
+if __name__ == '__main__':
+    process(learning_rate=0.01, batch_size=10, epochs=1)
     pass
-
-if __name__ == '__main__':    
-    process(0.01)
-    pass
-
-
-
